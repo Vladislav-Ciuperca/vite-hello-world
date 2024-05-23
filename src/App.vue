30fd2cc07460ac260d5c0c5ce9065464f0efda0c
Vladislav-Ciuperca <ciuperca37@gmail.com>
2024-05-23 23:56:02 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  components:{
+    HelloWorld,
+  },
+
+  data() {
+    return {
+      nomeApplicazione: "esempioProva 0.1"
+    }
+  },
+  methods:{
+
+  },
+  mounted(){
+    console.log("app caricata con successo")
+  }
+}
+
+</script>
+
+<!-- ============================================ -->
+
+<template>
+  <h1>Benvenuto in {{nomeApplicazione}}</h1>
+  <HelloWorld />
+</template>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,13 @@
+<script>
+export default{
+  data (){
+    return{
+      messaggino: 'sonon il figlio'
+    }
+  }
+}
+</script>
+
+<template>
+  HelloWorld dice {{messaggino}} !
+</template>
